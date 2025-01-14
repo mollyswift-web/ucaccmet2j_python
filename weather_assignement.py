69292095faff62de705576880da7b98060a7d5e5
m.swift@students.uu.nl <m.swift@students.uu.nl>
2025-01-14 17:05:04 +0100

    version 2 of seattle results

--- weather_assignement.py
+++ weather_assignement.py
@@ -1,31 +1,48 @@
 import json 
 import string
-seattle_results = {}
+import csv
+seattle_ = {}
 csv_seattle = ['Seattle,WA,GHCND:US1WAKG0038']
 seattle_station_code = 'GHCND:US1WAKG0038'
 months = [ '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 with open ('stations.csv', encoding = 'utf-8') as file:
    code_content = file.read() 
 
-seattle= []
+seattle_data= []
 with open ('precipitation.json', 'r') as file: 
     contents= json.load(file) 
     for element in contents: 
         if element['station'] == seattle_station_code:
-            seattle.append(element)
+            seattle_data.append(element)
 
     monthly_precipitation = []
-    total_monthly_precipitation = 0
     for month in months:
-        for  key in seattle:
+        total_monthly_precipitation = 0
+        for  key in seattle_data:
             if key['date'] >= f'2010-{month}-01' and key['date'] <= f'2010-{month}-31':
-                monthly_precipitation.append(key['value'])
                 total_monthly_precipitation = total_monthly_precipitation + key['value']
 
+       
         print(f'{month}', total_monthly_precipitation)
+        monthly_precipitation.append(total_monthly_precipitation)
+
+    total_year_precipitation = sum(monthly_precipitation)
     
-        seattle_results[month] = {
-        'total_monthly_precipitation' : total_monthly_precipitation, 
+    print(total_year_precipitation)
+    
+    relative_month = []
+    for number in monthly_precipitation:
+        total_year_precipitation = sum(monthly_precipitation)
+        relative_monthly_precipitation = number / total_year_precipitation
+        relative_month.append(relative_monthly_precipitation)
+    
+    print('total year precipitation', total_year_precipitation)
+    print('relative_monthly_precipitation',relative_month)
+
+seattle = {
+    'total_monthly_precipitation' : monthly_precipitation, 
+    'total_year_precipitation' : total_year_precipitation,
+    'relative_monthly_precipitation' : relative_month 
     }
 with open ('results.json', 'w') as file:
-    json.dump(seattle_results, file, indent = 4)
+     json.dump(seattle, file, indent = 4)
